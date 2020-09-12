48d1d4f476c647f083f4cc1b0fdb949c120f1eae
Deepak Kumar <deepakbravo9889014930@gmail.com>
2020-09-12 10:33:23 +0530

    final code updates

--- routes/routes.js
+++ routes/routes.js
@@ -4,12 +4,9 @@ const router = express.Router()
 
 const staticctrl = require('../controllers/static')
 
-// router.get('/',staticctrl.landingPage)
-router.get('/user1',staticctrl.user1Page)
-router.post('/user2',staticctrl.user2Page)
-// router.get('/contact',staticctrl.contactPage)
-// router.get('/about',staticctrl.aboutPage)
-// router.get('/signup',staticctrl.signupPage)
+
+// router.get('/user1',staticctrl.user1Page)
+// router.post('/user2',staticctrl.user2Page)
 
 
 module.exports = router
